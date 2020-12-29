86097ed8533ab49a779db25e3db6cd7c37781a45
fluorinedog <fluorinedog@gmail.com>
2020-12-29 17:12:39 +0800

    final
    Signed-off-by: fluorinedog <fluorinedog@gmail.com>

--- src/dynamic_bitset_ext.hpp
+++ src/dynamic_bitset_ext.hpp
@@ -3,4 +3,4 @@
 namespace boost_ext {
 const char* get_data(const boost::dynamic_bitset<>& bitset);
 char* get_data(boost::dynamic_bitset<>& bitset);
-}
+}    // namespace boost_ext
--- unittest/test_naive.cpp
+++ unittest/test_naive.cpp
@@ -1,5 +1,34 @@
 #include "gtest/gtest.h"
+#include "dynamic_bitset_ext.hpp"
 
-TEST(Naive, naive) {
+TEST(Naive, read) {
     ASSERT_EQ(42, 42);
+    int N = 1000;
+    const auto bitset = [N] {
+        boost::dynamic_bitset<> bitset(N);
+        for (int i = 0; i < N; ++i) {
+            bitset.set(i, i % 3);
+        };
+        return bitset;
+    }();
+    auto ptr = boost_ext::get_data(bitset);
+    for(int i = 0; i < N; ++i) {
+        auto bit = (ptr[i / 8] >> (i % 8)) & 0x1;
+        ASSERT_EQ(bit, i % 3 != 0);
+    }
+}
+
+TEST(Naive, write) {
+    ASSERT_EQ(42, 42);
+    int N = 1000;
+    boost::dynamic_bitset<> bitset(N);
+    auto ptr = boost_ext::get_data(bitset);
+    for(int i = 0; i < N; ++i) {
+        ptr[i / 8] |= (char)(1 << (i % 8));
+    }
+
+    for(int i = 0; i < N; ++i) {
+        bool bit = bitset[i];
+        ASSERT_EQ(bit, i % 3 != 0);
+    }
 }
